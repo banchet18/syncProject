5779851cd4d7448b0d52c82a2243a4ac2239b3c6
banchet18 <banjarac6@gmail.com>
2024-01-14 19:14:42 +0530

    c

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,7 +1,46 @@
 import "./App.css";
+import { Search, MapPin, Wind } from "react-feather";
 
 function App() {
-  return <div className="App">asdff</div>;
+  return (
+    <div className="app">
+      <h1>Weather App</h1>
+      <div className="input-wrapper">
+        <input type="text" />
+        <button>
+          <Search></Search>
+        </button>
+      </div>
+      <div className="content">
+        <div className="location d-flex">
+          <MapPin></MapPin>
+          <h2>
+            London <span>(GB)</span>
+          </h2>
+        </div>
+        <p className="datetext">2 october 2023</p>
+        <div className="weatherdesc">
+          <img src="" alt="" />
+          <h3>Clear sky</h3>
+        </div>
+        <div className="tempstats d-flex flex-c">
+          <h1>
+            43 <span>&deg;C</span>
+          </h1>
+          <h3>
+            Feels Like 44 <span>&deg;C</span>
+          </h3>
+        </div>
+        <div className="windstats d-flex">
+          <Wind></Wind>
+          <h3>Wind is 20 knots is 45&deg;</h3>
+        </div>
+        <div className="content">
+          <h4>No Data Found</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
