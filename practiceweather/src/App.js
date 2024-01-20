4b28d566ff7997bcd17d5d728a67d0b5a9f8fa5c
banchet18 <banjarac6@gmail.com>
2024-01-20 09:07:53 +0530

    c

--- practiceweather/src/App.js
+++ practiceweather/src/App.js
@@ -1,6 +1,7 @@
 import Search from "./components/Search";
 import Result from "./components/Result";
 import { useEffect, useState } from "react";
+import axios from "react-axios";
 
 const App = () => {
   const [search, setSearch] = useState("");
@@ -10,15 +11,26 @@ const App = () => {
     setSearch(value);
   };
 
-  const getWeatherData = () => {};
-
-  useEffect(() => {
-    getWeatherData();
-  }, [search]);
+  const searchWeather = () => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=a3c79758b7d82414f9b304fec4991587`
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className=" max-w-[1280px] mx-auto mt-2 p-3 ">
-      <Search searchData={search} eventhandler={changeSearch} />
+      <Search
+        searchData={search}
+        eventhandler={changeSearch}
+        searchWeather={searchWheatherHandler}
+      />
       <Result weatherData={weather} />
     </div>
   );
